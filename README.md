578977e956cf9929b9a6815aa6210f3aa2c067c0
Vyacheslav Polishchuk <vyacheslav.polishuk@iotechnologies.com>
2024-02-10 14:35:50 +0200

    feat(init) vite+tailwind+code-styling configured

---
+++ README.md
@@ -0,0 +1,4 @@
+# React + Vite + Graphql Kanban Board
+
+This app is a simple kanban board built with React, Vite, and GraphQL.
+With drag and drop functionality, it can be used for any type of kanban board.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <div className="w-screen h-screen bg-blue-800 flex justify-center items-center tetxt-4xl text-orange-500">
+      Hello
+    </div>
+  );
+};
+
+export default App;
